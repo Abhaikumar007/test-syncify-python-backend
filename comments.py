e8cf096edbd5e99f6016efb1b8249b0d87cbc3aa
Abhai Kumar <abhaikumar015@gmail.com>
2025-12-04 21:00:12 +0530

    ADDING THE COMMENTS PYTHON FILE

---
+++ comments.py
@@ -0,0 +1,23 @@
+
+"""
+print("Myself Pratik")
+print("i AM FORM MUMBAI")
+print("I like to play football")
+print("I like to travel")
+"""
+'''
+print("Myself Pratik")
+print("i AM FORM MUMBAI")
+print("I like to play football")
+print("I like to travel")
+'''
+name="Pratik"
+print(name)
+num=25
+print(num)
+name="John"
+Name="Jane"
+NAME="Jack"
+print(name)
+print(Name)
+print(NAME)
