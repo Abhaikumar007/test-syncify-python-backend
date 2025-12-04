340d6bc3c69a26648d12967e4d4ba11d0ee47c77
Abhai Kumar <abhaikumar015@gmail.com>
2025-12-04 20:34:46 +0530

    First commit

---
+++ print.py
@@ -0,0 +1,21 @@
+print("This is our first program in python using double quotes")
+print("This is our first program in python using single quotes")
+print("He said,'python is awesome'")
+print('She replied,"Indeed it is "')
+print(42)
+print("The answer is",42)
+#this is called the seperator default seperator
+#we can use any string as seperator
+#we can use single quotes or double quotes for sep values
+print("pratik","loves","coding",sep="-")
+#prniting without newline
+#we can use single quotes or double quotes
+# we can use any string as end value
+print("Helo",end=" ")
+print("world")
+
+#give an empty print
+print()
+
+print("hello\nAasim") #creates new line
+print("Hello\tAasim") #creates tab space
