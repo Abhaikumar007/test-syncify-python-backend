cbd64b765c15600c7c730367de0e76aafa9b4940
Abhai Kumar <abhaikumar015@gmail.com>
2025-12-04 21:52:08 +0530

    add variables

---
+++ variable.py
@@ -0,0 +1,17 @@
+a="Pratik"
+b="Aasim"
+print(a)
+print(b)
+print("After swaping--------")
+#method 1
+
+temp=a
+a=b
+b=temp
+print("temp ------------>",temp)
+print("b----------------->",b)
+print("a ---------->"a)
+#Method 2
+a,b=b,a
+print(a)
+print(b)
